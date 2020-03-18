41d38c5703c0703ffbdb992edfe6c439b6b9eab4
elsiff <elsiff1002@gmail.com>
2020-03-18 16:12:31 +0900

    Change style of the scrollbar

--- src/App.vue
+++ src/App.vue
@@ -63,4 +63,19 @@
 </script>
 
 <style>
+    ::-webkit-scrollbar {
+        width: 5px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: #f1f1f1;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background: #888;
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: #555;
+    }
 </style>
