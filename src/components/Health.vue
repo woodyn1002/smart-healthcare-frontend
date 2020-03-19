055fbdf58a30f95a2862d289130520196ed72124
elsiff <elsiff1002@gmail.com>
2020-03-19 21:29:58 +0900

    Move codes of calculating health data values to backend api

--- src/components/Health.vue
+++ src/components/Health.vue
@@ -7,26 +7,21 @@
             <b-row>
                 <b-col sm="5">
                     <b-card class="mb-3" header="기초대사량">
-                        <b-card-text class="display-4" v-if="bmr !== undefined">{{ bmr }} kcal</b-card-text>
+                        <b-card-text class="display-4" v-if="healthData.bmr">{{ healthData.bmr }} kcal</b-card-text>
                         <b-card-text v-else>성별과 생일, 신장, 체중 정보가 필요합니다.</b-card-text>
                     </b-card>
 
                     <b-card class="mb-3" header="비만도">
-                        <template v-if="bmi !== undefined">
-                            <b-card-text class="display-4">{{ bmi }}</b-card-text>
+                        <template v-if="healthData.bmi">
+                            <b-card-text class="display-4">{{ healthData.bmi }}</b-card-text>
 
                             <h6>
                                 현재 체중
-                                <b-badge v-if="bmiState === bmiStates.underweight" variant="warning">저체중</b-badge>
-                                <b-badge v-else-if="bmiState === bmiStates.normal" variant="success">정상 체중</b-badge>
-                                <b-badge v-else-if="bmiState === bmiStates.overweight" variant="warning">과체중</b-badge>
-                                <b-badge v-else-if="bmiState === bmiStates.obese" variant="danger">비만</b-badge>
-                                <b-badge v-else-if="bmiState === bmiStates.extremelyObese" variant="danger">고도비만
-                                </b-badge>
+                                <b-badge :variant="bmiStateVariant">{{ bmiStateText }}</b-badge>
                             </h6>
                             <b-progress class="mb-2">
-                                <b-progress-bar :label="String(bmi)" :value="bmi - 15" :variant="bmiStateVariant"
-                                                max="20"></b-progress-bar>
+                                <b-progress-bar :label="String(healthData.bmi)" :value="healthData.bmi - 15"
+                                                :variant="bmiStateVariant" max="20"></b-progress-bar>
                             </b-progress>
                             <h6>정상 체중</h6>
                             <b-progress>
@@ -57,7 +52,7 @@
                                 <span id="waist">
                                     <template v-if="data.value">
                                         {{ data.value }}cm
-                                        <template v-if="abnormalWaist">
+                                        <template v-if="healthData.abnormalFields.includes('waist')">
                                             <b-badge class="ml-1" variant="danger">비정상</b-badge>
                                         </template>
                                     </template>
@@ -71,7 +66,7 @@
                                 <span id="blood-pressure">
                                     <template v-if="data.value && data.value.min && data.value.max">
                                         {{ data.value.min }}~{{ data.value.max }}mmHg
-                                        <template v-if="abnormalBloodPressure">
+                                        <template v-if="healthData.abnormalFields.includes('bloodPressure')">
                                             <b-badge class="ml-1" variant="danger">비정상</b-badge>
                                         </template>
                                     </template>
@@ -83,7 +78,7 @@
                                 <span id="neutral-fat">
                                     <template v-if="data.value">
                                         {{ data.value }}mg/DI
-                                        <template v-if="abnormalNeutralFat">
+                                        <template v-if="healthData.abnormalFields.includes('neutralFat')">
                                             <b-badge class="ml-1" variant="danger">비정상</b-badge>
                                         </template>
                                     </template>
@@ -95,7 +90,7 @@
                                 <span id="hdl-cholesterol">
                                     <template v-if="data.value">
                                         {{ data.value }}mg/DI
-                                        <template v-if="abnormalHdlCholesterol">
+                                        <template v-if="healthData.abnormalFields.includes('hdlCholesterol')">
                                             <b-badge class="ml-1" variant="danger">비정상</b-badge>
                                         </template>
                                     </template>
@@ -109,7 +104,7 @@
                                 <span id="fasting-blood-sugar">
                                     <template v-if="data.value">
                                         {{ data.value }}mg/DI
-                                        <template v-if="abnormalFastingBloodSugar">
+                                        <template v-if="healthData.abnormalFields.includes('fastingBloodSugar')">
                                             <b-badge class="ml-1" variant="danger">비정상</b-badge>
                                         </template>
                                     </template>
@@ -136,7 +131,6 @@
 
 <script>
     import HealthEditModal from "@/components/HealthEditModal";
-    import moment from "moment";
 
     export default {
         name: "health",
@@ -172,100 +166,37 @@
                     neutralFatMedicine: undefined,
                     hdlCholesterol: undefined,
                     fastingBloodSugar: undefined,
-                    fastingBloodSugarMedicine: undefined
+                    fastingBloodSugarMedicine: undefined,
+                    age: undefined,
+                    bmr: undefined,
+                    bmi: undefined,
+                    bmiState: undefined,
+                    abnormalFields: []
                 },
                 bmiStates: {
-                    underweight: 0,
-                    normal: 1,
-                    overweight: 2,
-                    obese: 3,
-                    extremelyObese: 4
+                    underweight: 'underweight',
+                    normal: 'normal',
+                    overweight: 'overweight',
+                    obese: 'obese',
+                    extremelyObese: 'extremelyObese'
                 }
             }
         },
         computed: {
-            age() {
-                if (!this.healthData.birthdate || this.healthData.birthdate.date) return undefined;
-
-                return moment().year() - moment(this.healthData.birthdate.date).year() + 1;
-            },
-            bmr() {
-                if (!this.age) return undefined;
-                if (!this.healthData.weight) return undefined;
-                if (!this.healthData.height) return undefined;
-
-                let sex = this.healthData.sex;
-                let weight = this.healthData.weight;
-                let height = this.healthData.height;
-
-                if (sex === 'male')
-                    return 66.47 + (13.75 * weight) + (5 * height) - (6.76 * this.age);
-                else if (sex === 'female')
-                    return 65.51 + (9.56 * weight) + (1.85 * height) - (4.68 * this.age);
-                else
-                    return undefined;
-            },
-            bmi() {
-                if (!this.healthData.weight) return undefined;
-                if (!this.healthData.height) return undefined;
-
-                let weight = this.healthData.weight;
-                let heightInMeters = this.healthData.height / 100;
-
-                let value = weight / (heightInMeters * heightInMeters);
-                return Math.floor(value * 100) / 100;
-            },
-            bmiState() {
-                if (this.bmi < 18.5) return this.bmiStates.underweight;
-                else if (this.bmi < 23) return this.bmiStates.normal;
-                else if (this.bmi < 25) return this.bmiStates.overweight;
-                else if (this.bmi < 30) return this.bmiStates.obese;
-                else return this.bmiStates.extremelyObese;
+            bmiStateText() {
+                let bmiState = this.healthData.bmiState;
+                if (bmiState === this.bmiStates.underweight) return '저체중';
+                else if (bmiState === this.bmiStates.normal) return '정상 체중';
+                else if (bmiState === this.bmiStates.overweight) return '과체중';
+                else if (bmiState === this.bmiStates.obese) return '비만';
+                else return '고도비만';
             },
             bmiStateVariant() {
-                if (this.bmiState <= this.bmiStates.underweight) return 'warning';
-                else if (this.bmiState === this.bmiStates.normal) return 'success';
-                else if (this.bmiState === this.bmiStates.overweight) return 'warning';
+                let bmiState = this.healthData.bmiState;
+                if (bmiState === this.bmiStates.underweight) return 'warning';
+                else if (bmiState === this.bmiStates.normal) return 'success';
+                else if (bmiState === this.bmiStates.overweight) return 'warning';
                 else return 'danger';
-            },
-            abnormalWaist() {
-                if (!this.healthData.waist) return false;
-
-                let sex = this.healthData.sex;
-                if (sex === 'male')
-                    return this.healthData.waist >= 90;
-                else if (sex === 'female')
-                    return this.healthData.waist >= 85;
-                else
-                    return false;
-            },
-            abnormalBloodPressure() {
-                if (!this.healthData.bloodPressure) return false;
-                if (!this.healthData.bloodPressure.min) return false;
-                if (!this.healthData.bloodPressure.max) return false;
-
-                return this.healthData.bloodPressure.min >= 85 || this.healthData.bloodPressure.max >= 130;
-            },
-            abnormalNeutralFat() {
-                if (!this.healthData.neutralFat) return false;
-
-                return this.healthData.neutralFat >= 150;
-            },
-            abnormalHdlCholesterol() {
-                if (!this.healthData.hdlCholesterol) return false;
-
-                let sex = this.healthData.sex;
-                if (sex === 'male')
-                    return this.healthData.hdlCholesterol < 40;
-                else if (sex === 'female')
-                    return this.healthData.hdlCholesterol < 50;
-                else
-                    return false;
-            },
-            abnormalFastingBloodSugar() {
-                if (!this.healthData.fastingBloodSugar) return false;
-
-                return this.healthData.fastingBloodSugar >= 100;
             }
         },
         methods: {
@@ -284,6 +215,10 @@
                 neutralFat: 160,
                 hdlCholesterol: 30,
                 fastingBloodSugar: 110,
+                bmr: 1999,
+                bmi: 24.69,
+                bmiState: 'overweight',
+                abnormalFields: ['neutralFat', 'hdlCholesterol', 'fastingBloodSugar']
             };
         }
     }
