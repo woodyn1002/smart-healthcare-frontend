22b371840b682a05c2416ead52a3dca53fbdd38c
elsiff <elsiff1002@gmail.com>
2020-03-20 18:07:52 +0900

    Fix issue of login

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,7 +6,7 @@ const API_URL = 'http://localhost:3000/v1';
 export function authHeader() {
     const user = JSON.parse(localStorage.getItem('user'));
     if (user && user.token) {
-        return {Authorization: 'Bearer ' + user.token};
+        return {'x-access-token': user.token};
     } else {
         return {};
     }
