855a249160109b5f3dd1d5e21e2889e11ba7c5ce
elsiff <elsiff1002@gmail.com>
2020-03-20 17:16:11 +0900

    Refactor auth service to exported functions

--- src/services/auth.js
+++ src/services/auth.js
@@ -3,34 +3,30 @@ import qs from "qs";
 
 const API_URL = 'http://localhost:3000/v1';
 
-class AuthService {
-    authHeader() {
-        const user = JSON.parse(localStorage.getItem('user'));
-        if (user && user.token) {
-            return {Authorization: 'Bearer ' + user.token};
-        } else {
-            return {};
-        }
-    }
-
-    register(body) {
-        return axios.post(API_URL + '/auth/register', qs.stringify(body))
-            .then(response => Promise.resolve(response.data))
-            .catch(error => Promise.reject(error.response.data));
-    }
-
-    login(body) {
-        return axios.post(API_URL + '/auth/login', qs.stringify(body))
-            .then(response => {
-                localStorage.setItem('user', JSON.stringify(response.data));
-                return Promise.resolve(response.data);
-            })
-            .catch(err => Promise.reject(err.response.data));
-    }
-
-    logout() {
-        localStorage.removeItem('user');
+export function authHeader() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user && user.token) {
+        return {Authorization: 'Bearer ' + user.token};
+    } else {
+        return {};
     }
 }
 
-export default new AuthService();
+export function register(body) {
+    return axios.post(API_URL + '/auth/register', qs.stringify(body))
+        .then(response => Promise.resolve(response.data))
+        .catch(error => Promise.reject(error.response.data));
+}
+
+export function login(body) {
+    return axios.post(API_URL + '/auth/login', qs.stringify(body))
+        .then(response => {
+            localStorage.setItem('user', JSON.stringify(response.data));
+            return Promise.resolve(response.data);
+        })
+        .catch(err => Promise.reject(err.response.data));
+}
+
+export function logout() {
+    localStorage.removeItem('user');
+}
--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,4 +1,4 @@
-import AuthService from "../services/auth";
+import * as AuthService from "../services/auth";
 
 const user = JSON.parse(localStorage.getItem('user'));
 const initialState = (user) ? {status: {loggedIn: true}, user} : {status: {}, user: null};
